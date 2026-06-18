5f1bc6b33d924c4cb01a7c1ddc2096397e7aa785
Jaelson Kaciel <111665673+toddy007@users.noreply.github.com>
2026-06-18 23:21:18 +0000

    fix(routes): solving no using main url in routes

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,3 +1,5 @@
-export const BOTLIST_URL = 'api/botlist/';
+import { API_URL } from '../index.js';
+
+export const BOTLIST_URL = API_URL + 'api/botlist/';
 export const DATA_URL = BOTLIST_URL + 'data';
 export const CHECK_VOTE_URL = BOTLIST_URL + 'check_vote';
