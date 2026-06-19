b0aa82a3925f7a889cb4746a5a929c30865e4749
toddy007 <jaelsonkaciel4@gmail.com>
2026-06-18 21:57:19 -0300

    fix(index): fixing runtime error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export const API_URL = 'https://unionlab.squareweb.app/';
-
 export * from './utils/routes.js';
 
 export type * from './types/global.js';
--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,5 +1,4 @@
-import { API_URL } from '../index.js';
-
+export const API_URL = 'https://unionlab.squareweb.app/';
 export const BOTLIST_URL = API_URL + 'api/botlist/';
 export const DATA_URL = BOTLIST_URL + 'data';
 export const CHECK_VOTE_URL = BOTLIST_URL + 'check_vote';
