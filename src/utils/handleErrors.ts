61bbb15e00d82948c52e4fd6358a202676ac8965
Jaelson Kaciel <111665673+toddy007@users.noreply.github.com>
2026-06-20 15:26:59 +0000

    feat(errors): adding errors handler

---
+++ src/utils/handleErrors.ts
@@ -0,0 +1,10 @@
+import { AxiosError } from "axios";
+import { UnionAPISdkError } from "./unionAPISdkError";
+
+export function handleErrors(error: AxiosError) {
+    const errorResponse = error.response;
+    const errorData = errorResponse?.data as { error?: string, retryAfter?: number };
+    const retryAfter = errorData?.retryAfter;
+
+    return new UnionAPISdkError(`${errorResponse?.status ?? '???'}: ${errorData?.error ?? 'Unknown error'} ${retryAfter ? `( retry-after: ${retryAfter} segundos )` : ''}`);
+}
