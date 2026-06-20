13c468d095860340ffc55f3c544e8918729b1618
Jaelson Kaciel <111665673+toddy007@users.noreply.github.com>
2026-06-20 15:45:29 +0000

    fix(errors): can be undefined

--- src/utils/handleErrors.ts
+++ src/utils/handleErrors.ts
@@ -3,7 +3,7 @@ import { UnionAPISdkError } from "./unionAPISdkError.js";
 
 export function handleErrors(error: AxiosError) {
     const errorResponse = error.response;
-    const errorData = errorResponse?.data as { error?: string, retryAfter?: number };
+    const errorData = errorResponse?.data as { error?: string, retryAfter?: number } | undefined;
     const retryAfter = errorData?.retryAfter;
 
     return new UnionAPISdkError(`${errorResponse?.status ?? '???'}: ${errorData?.error ?? 'Unknown error'} ${retryAfter ? `( retry-after: ${retryAfter} segundos )` : ''}`);
